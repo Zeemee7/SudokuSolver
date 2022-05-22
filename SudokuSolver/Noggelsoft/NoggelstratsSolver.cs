f04c2d1d2593b3f29186b7323c3abdfca647602c
Noggels <noggels@outlook.com>
2022-05-22 15:47:38 +0200

    Erste Version NoggelstratsSolver.cs

---
+++ SudokuSolver/Noggelsoft/NoggelstratsSolver.cs
@@ -0,0 +1,11 @@
+﻿namespace SudokuSolver.Noggelsoft;
+
+public class NoggelstratsSolver : ISudokuSolver
+{
+    public ISudoku Solve(ISudoku problem)
+    {
+        problem.SetValue(1,9,1);
+        new ConsoleSudokuUserInterface().DisplaySudoku(problem);
+        return problem;
+    }
+}
